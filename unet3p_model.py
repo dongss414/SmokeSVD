a5f3da02b0207909899a3d2a2926666c21009170
dongss414 <156882382+dongss414@users.noreply.github.com>
2025-05-23 20:14:22 +0800

    Update unet3p_model.py

--- unet3p_model.py
+++ unet3p_model.py
@@ -104,30 +104,20 @@ class UNet3DMultiView16(nn.Module):
             rho = only_rotated_density_field(density[0, i, :, :, :], self.angleList[i])
             new_density[0,i,:,:,:] = rho
 
-        # new_density = F.interpolate(new_density, size=(96, 176, 96), mode="trilinear", align_corners=False)
+
         newList = []
 
-        # density = only_rotated_density_field(density[0,0,:,:,:],angle)
-            # print(density.shape)
-        #     newList.append(density)
-        # density = torch.cat(newList,dim=1)
-        # print(density.shape)
-
-
-        # density = torch.cat([img1,img2], dim=1)
-        # x00 = self.input(new_density)
-        # x00 = self.sigmoid(x00)
 
         x00 = self.conv_input_00(new_density)# [2,64^3]->[f[0],64^3]
-        # print(x00.shape)
+
         x10 = self.res_00_10(x00)# [f[0],32^3]->[f[1],32^3]
-        # print(x10.shape)
+
         x20 = self.res_10_20(x10)# [f[1],16^3]->[f[2],16^3]
-        # print(x20.shape)
+
         x30 = self.res_20_30(x20)# [f[2],8^3]->[f[3],8^3]
-        # print(x30.shape)
+
         x40 = self.res_30_40(x30)# [f[3],4^3]->[f[4],4^3]
-        # print(x40.shape)
+
 
         x41 = x40
         x31 = self.res_31_0(torch.cat((self.res43(self.up2(x41)), self.res33(x30), self.res23(self.down2(x20)),
@@ -138,46 +128,26 @@ class UNet3DMultiView16(nn.Module):
             self.res11(x10), self.res01(self.down2(x00))), 1))# [f[0]*5,11,128^2]; from [x41,x31,x21,x10,x00]
         x01 = self.res_01_0(torch.cat((self.res40(self.up16(x41)), self.res30(self.up8(x31)), self.res20(self.up4(x21)),
             self.res10(self.up2(x11)), self.res00(x00)), 1))# [f[0]*5,11,256^2]; from [x41,x31,x21,x11,x00]
-        # print(x01.shape)
-        # print(x11.shape)
-        # print(x21.shape)
-        # print(x31.shape)
-        # print(x41.shape)
-        # short cut
-        x01 = self.conv_01_1(x01)  # + density# [f[0]*5,64^3]->[1,64^3]
 
-        # x01 = torch.clamp(x01, min=0.0, max=1.0)
-        # x01 = self.sigmoid(x01)
-        # x01 = self.relu(x01)
+        x01 = self.conv_01_1(x01)  # + density# [f[0]*5,64^3]->[1,64^3]
 
 
 
         x11 = self.upscore2(x11)
         x11 = self.conv_11_1(x11)  # + self.down2(density)# [f[0]*5,32^3]->[1,32^3]
-        # x11 = torch.clamp(x11, min=0.0, max=1.0)
-        # x11 = self.sigmoid(x11)
-        # x11 = self.relu(x11)
 
 
 
-        # x11 = self.upscore2(x11)
+
 
         x21 = self.upscore3(x21)
 
         x21 = self.conv_21_1(x21)  # + self.down4(density)# [f[0]*5,16^3]->[1,16^3]
-        # x21 = torch.clamp(x21, min=0.0, max=1.0)
-        # x21 = self.upscore3(x21)
-        # x21 = self.sigmoid(x21)
-        # x21 = self.relu(x21)
 
 
         x31 = self.upscore4(x31)
 
         x31 = self.conv_31_1(x31)  # + self.down8(density)# [f[0]*5,8^3]->[1,8^3]
-        # x31 = torch.clamp(x31, min=0.0, max=1.0)
-        # x31 = self.upscore4(x31)
-        # x31 = self.sigmoid(x31)
-        # x31 = self.relu(x31)
 
 
 
@@ -186,23 +156,8 @@ class UNet3DMultiView16(nn.Module):
         x41 = self.upscore5(x41)
 
         x41 = self.conv_41_1(x41)  # + self.down16(density)# [f[0]*5,4^3]->[1,4^3]
-        # x41 = torch.clamp(x41, min=0.0, max=1.0)
-        # x41 = self.upscore5(x41)
-        # x41 = self.sigmoid(x41)
-        # x41 = self.relu(x41)
 
 
-
-        # x01 = self.MinMaxNorm(x01)
-        # x11 = self.MinMaxNorm(x11)
-        # x21 = self.MinMaxNorm(x21)
-        # x31 = self.MinMaxNorm(x31)
-        # x41 = self.MinMaxNorm(x41)
-        # x01 = F.interpolate(x01, size=(100, 178, 100), mode="trilinear", align_corners=False)
-        # x41 = F.interpolate(x41, size=(100, 178, 100), mode="trilinear", align_corners=False)
-        # x41 = F.interpolate(x41, size=(100, 178, 100), mode="trilinear", align_corners=False)
-        # x41 = F.interpolate(x41, size=(100, 178, 100), mode="trilinear", align_corners=False)
-        # x41 = F.interpolate(x41, size=(100, 178, 100), mode="trilinear", align_corners=False)
 
         return x01, x11, x21, x31, x41
 
@@ -211,13 +166,9 @@ class UNet3D_trainVelocity(nn.Module):
     # Network Architecture is exactly same as UNet+++ and 3D
     def __init__(self,inchannels=2,f=[8, 16, 32, 64, 128]):
         super(UNet3D_trainVelocity, self).__init__()
-        # f = [64, 128, 256, 512, 1024]
-        # f = [16, 32, 32, 64, 64]
-        # f = [16, 32, 64, 128, 256]
-        # f = [32, 64, 128, 256, 512]
-        # f = f
+
         # upsample and downsample
-        # self.up2D_3D = nn.Upsample(scale_factor=(64,1,1), mode='trilinear')
+
         self.up2 = nn.Upsample(scale_factor=(2,2,2), mode='trilinear')
         self.up4 = nn.Upsample(scale_factor=(4,4,4), mode='trilinear')
         self.up8 = nn.Upsample(scale_factor=(8,8,8), mode='trilinear')
@@ -288,9 +239,7 @@ class UNet3D_trainVelocity(nn.Module):
         return (x-torch.min(x))/(torch.max(x)-torch.min(x)+eps)
 
     def forward(self, density, randomview=0):
-        # density0,1: [1,1,1,64^2]
-        # density = self.up2D_3D(front_alpha)
-        # density = torch.cat([img1,img2], dim=1)
+
         x00 = self.conv_input_00(density)# [2,64^3]->[f[0],64^3]
         x10 = self.res_00_10(x00)# [f[0],32^3]->[f[1],32^3]
         x20 = self.res_10_20(x10)# [f[1],16^3]->[f[2],16^3]
@@ -312,25 +261,18 @@ class UNet3D_trainVelocity(nn.Module):
 
         x11 = self.upscore2(x11)
         x11 = self.conv_11_1(x11)# + self.down2(density)# [f[0]*5,32^3]->[1,32^3]
-        # x11 = self.upscore2(x11)
+
 
         x21 = self.upscore3(x21)
         x21 = self.conv_21_1(x21)# + self.down4(density)# [f[0]*5,16^3]->[1,16^3]
-        # x21 = self.upscore3(x21)
+
 
         x31 = self.upscore4(x31)
         x31 = self.conv_31_1(x31)# + self.down8(density)# [f[0]*5,8^3]->[1,8^3]
-        # x31 = self.upscore4(x31)
 
         x41 = self.upscore5(x41)
         x41 = self.conv_41_1(x41)# + self.down16(density)# [f[0]*5,4^3]->[1,4^3]
-        # x41 = self.upscore5(x41)
 
-        # x01 = self.MinMaxNorm(x01)
-        # x11 = self.MinMaxNorm(x11)
-        # x21 = self.MinMaxNorm(x21)
-        # x31 = self.MinMaxNorm(x31)
-        # x41 = self.MinMaxNorm(x41)
         return x01, x11, x21, x31, x41
 
 
